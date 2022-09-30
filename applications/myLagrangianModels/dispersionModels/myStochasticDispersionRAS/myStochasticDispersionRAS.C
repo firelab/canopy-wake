c986fd455f5586b9d107da652cfd35d3553f339a
latwood <pianotocador@gmail.com>
2022-09-29 22:46:54 -0700

    This version of the Lagrangian particle turbulence models, myStochasticDispersionRAS, was to edit the default OpenFOAM 8 stochasticDispersionRAS model with the simplest model described in Bailey 2017 "Numerical Considerations for Lagrangian Stochastic Dispersion Models: Eliminating Rogue Trajectories, and the Importance of Numerical Accuracy". The more complex particle turbulence models described in Bailey 2017 are supposed to be way better than this model, but this model was the simplest and easiest to code up, coding it up didn't require editing the next levels up in the inheritance structure of the code. And this model was already a ton better than the default particle turbulence models of OpenFOAM 8. This should be the final version of the dispersion model that was actually used in the ember project throughout the ember project.

--- applications/myLagrangianModels/dispersionModels/myStochasticDispersionRAS/myStochasticDispersionRAS.C
+++ applications/myLagrangianModels/dispersionModels/myStochasticDispersionRAS/myStochasticDispersionRAS.C
@@ -73,46 +73,37 @@ Foam::vector Foam::myStochasticDispersionRAS<CloudType>::update
 {
     Random& rnd = this->owner().rndGen();
 
-    const scalar cps = 0.16432;
+    const scalar C_0 = 4;
 
     const scalar k = this->kPtr_->primitiveField()[celli];
     const scalar epsilon =
         this->epsilonPtr_->primitiveField()[celli] + rootVSmall;
-
-    const scalar UrelMag = mag(U - Uc - UTurb);
-
-    const scalar tTurbLoc =
-        min(k/epsilon, cps*pow(k, 1.5)/epsilon/(UrelMag + small));
-
-
-    // Parcel is perturbed by the turbulence
-    if (dt < tTurbLoc)
+    
+    const scalar sigma2 = sqrt(2*k/3.0);    // the variance, so stddev^2
+    
+    const vector randNum( rnd.scalarNormal(), rnd.scalarNormal(), rnd.scalarNormal() );
+    
+    //std::cout << "dt = \"" << dt << "\"" << std::endl;
+    //std::cout << "tTurb = \"" << tTurb << "\"" << std::endl;
+    //std::cout << "k = \"" << k << "\", epps = \"" << epsilon << "\"" << std::endl;
+    //std::cout << "Uc = (" << Uc[0] << "," << Uc[1] << "," << Uc[2] << ")" << std::endl;
+    //std::cout << "U = (" << U[0] << "," << U[1] << "," << U[2] << ")" << std::endl;
+    //std::cout << "UTurb = (" << UTurb[0] << "," << UTurb[1] << "," << UTurb[2] << ")" << std::endl;
+    //std::cout << "randNum = (" << randNum[0] << "," << randNum[1] << "," << randNum[2] << ")" << std::endl;
+    
+    // this is the equation for homogeneous isotropic turbulence as an explicit scheme from https://baileylab.ucdavis.edu/publications/Bailey_2017_BLM.pdf
+    // term1 is the explicit scheme term, term2 is the drift term, term3 is what I call the wiggle term
+    // notice that sigma2 is the variance, or stddev^2 (sigma is stddev), the weiner process has mean 0 variance dt, so it is sqrt(dt) not dt in the eqn
+    UTurb = UTurb - C_0*epsilon/(2*sigma2)*UTurb*dt + sqrt(C_0*epsilon*dt)*randNum;
+    
+    tTurb += dt;
+    if ( UTurb[0] == 0 && UTurb[1] == 0 && UTurb[2] )
     {
-        tTurb += dt;
-
-        //if (tTurb > tTurbLoc)
-        //{
-            tTurb = 0;
-
-            const scalar sigma = sqrt(2*k/3.0);
-
-            // Calculate a random direction dir distributed uniformly
-            // in spherical coordinates
-
-            const scalar theta = rnd.scalar01()*twoPi;
-            const scalar u = 2*rnd.scalar01() - 1;
-
-            const scalar a = sqrt(1 - sqr(u));
-            const vector dir(a*cos(theta), a*sin(theta), u);
-
-            UTurb = sigma*mag(rnd.scalarNormal())*dir;
-        //}
+        tTurb = 0;
     }
-    else
-    {
-        tTurb = great;
-        UTurb = Zero;
-    }
+    
+    //std::cout << "new tTurb = \"" << tTurb << "\"" << std::endl;
+    //std::cout << "new UTurb = (" << UTurb[0] << "," << UTurb[1] << "," << UTurb[2] << ")\n" << std::endl;
 
     return Uc + UTurb;
 }
